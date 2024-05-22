5a4238df8e2b6d98cff1d78236953048f23c6afc
OmOgale <123871192+OmOgale@users.noreply.github.com>
2024-05-22 03:06:46 -0400

    Temp change to CORS

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import { config } from "dotenv";
 import express from "express";
 import logger from "./middleware/logEvents";
 import path from "path";
-import cors from 'cors';
+import cors from "cors";
 import { corsOptions } from "./config/corsOptions";
 import mongoose from "mongoose";
 import connectDB from "./config/dbConn";
@@ -10,7 +10,7 @@ import rootRouter from "./routes/root";
 import blogPostRouter from "./routes/api/blogPosts";
 import userRouter from "./routes/api/users";
 import errorHandler from "./middleware/errorHandler";
-import cookieParser  from "cookie-parser";
+import cookieParser from "cookie-parser";
 import helmet from "helmet";
 
 config();
@@ -29,40 +29,40 @@ connectDB();
 app.use(logger);
 
 // Cross Origin Resource Sharing
-app.use(cors(corsOptions));
+app.use(cors({ origin: "*" })); // TODO: change to corsOptions
 
 // built-in middleware to handle urlencoded form data
 app.use(express.urlencoded({ extended: false }));
 
-// built-in middleware for json 
+// built-in middleware for json
 app.use(express.json());
 
 //middleware for cookies
 app.use(cookieParser());
 
 //serve static files
-app.use('/', express.static(path.join(__dirname, '/public')));
+app.use("/", express.static(path.join(__dirname, "/public")));
 
 // routes
-app.use('/', rootRouter);
+app.use("/", rootRouter);
 
-app.use('/blog-posts', blogPostRouter);
-app.use('/users', userRouter);
+app.use("/blog-posts", blogPostRouter);
+app.use("/users", userRouter);
 
-app.all('*', (req, res) => {
-    res.status(404);
-    if (req.accepts('html')) {
-        res.sendFile(path.join(__dirname, 'views', '404.html'));
-    } else if (req.accepts('json')) {
-        res.json({ "error": "404 Not Found" });
-    } else {
-        res.type('txt').send("404 Not Found");
-    }
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ error: "404 Not Found" });
+  } else {
+    res.type("txt").send("404 Not Found");
+  }
 });
 
 app.use(errorHandler);
 
-mongoose.connection.once('open', () => {
-    console.log('Connected to MongoDB');
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDB");
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 });
